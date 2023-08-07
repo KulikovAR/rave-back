f72020b203dbbb4f2adf76e3ba5dc3cb4b6393ce
SashkaBackEnd <106818844+SashkaBackEnd@users.noreply.github.com>
2023-08-07 03:44:59 +0300

    Добавил статус коды

--- app/Docs/LessonController.php
+++ app/Docs/LessonController.php
@@ -56,6 +56,18 @@ class LessonController
      *          response="404",
      *          ref="#/components/responses/404"
      *      ),
+     *      @OA\Response(
+     *          response="403",
+     *          ref="#/components/responses/403"
+     *      ),
+     *      @OA\Response(
+     *          response="422",
+     *          ref="#/components/responses/422"
+     *      ),
+     *     @OA\Response(
+     *          response="401",
+     *          ref="#/components/responses/401"
+     *      ),
      * )
      */
     public function index() {
