e72fafe5b6bb07ee9213ee8367dcd026030ea1ec
SashkaBackEnd <106818844+SashkaBackEnd@users.noreply.github.com>
2023-08-07 03:50:00 +0300

    Форматирование

--- database/factories/LessonFactory.php
+++ database/factories/LessonFactory.php
@@ -17,9 +17,9 @@ class LessonFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => $this->faker->realText(100),
-            'description' => $this->faker->realText(),
-            'video_path' => url('videos/test_video.mp4'),
+            'title'        => $this->faker->realText(100),
+            'description'  => $this->faker->realText(),
+            'video_path'   => url('videos/test_video.mp4'),
             'preview_path' => url('previews/test_preview.mp4'),
             'announc_date' => $this->faker->date()
         ];
