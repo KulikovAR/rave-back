dcf3b362de9a19bb1534c2a68bad19a0fcde3293
SashkaBackEnd <106818844+SashkaBackEnd@users.noreply.github.com>
2023-08-16 16:04:07 +0300

    Правка uuid

--- app/Models/LessonAdditionalData.php
+++ app/Models/LessonAdditionalData.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LessonAdditionalData extends Model
 {
-    use HasFactory, HasUlids;
+    use HasFactory, HasUuids;
 
     public function lessons(): HasMany
     {
