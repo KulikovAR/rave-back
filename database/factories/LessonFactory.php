c10f0cb009f432f289ccc9223afa575d99508141
SashkaBackEnd <106818844+SashkaBackEnd@users.noreply.github.com>
2023-08-21 13:05:07 +0300

    Поменял формат видео в сидере

--- database/factories/LessonFactory.php
+++ database/factories/LessonFactory.php
@@ -20,7 +20,7 @@ class LessonFactory extends Factory
             'title'        => $this->faker->realText(100),
             'description'  => $this->faker->realText(),
             'video_path'   => url('videos/video1.mp4'),
-            'preview_path' => url('previews/test_preview.png'),
+            'preview_path' => url('previews/test_preview.webm'),
             'announc_date' => $this->faker->date()
         ];
     }
