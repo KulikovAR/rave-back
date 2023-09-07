25c5cd008be8886cde1d371ef02717ca039a7702
SashkaBackEnd <106818844+SashkaBackEnd@users.noreply.github.com>
2023-09-07 19:25:57 +0300

    Добавил уроки в админку

--- app/Filament/Resources/LessonResource.php
+++ app/Filament/Resources/LessonResource.php
@@ -3,13 +3,11 @@
 namespace App\Filament\Resources;
 
 use App\Filament\MenuTitles;
-use App\Filament\Resources\AnnounceResource\Pages;
-use App\Filament\Resources\AnnounceResource\RelationManagers;
-use App\Models\Announce;
+use App\Filament\Resources\LessonResource\Pages;
+use App\Filament\Resources\LessonResource\RelationManagers;
+use App\Models\Lesson;
 use App\Models\Tag;
 use Filament\Forms;
-use Filament\Forms\Components\Checkbox;
-use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
@@ -21,16 +19,21 @@ use Filament\Tables;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 
-class AnnounceResource extends Resource
+class LessonResource extends Resource
 {
-    protected static ?string $model = Announce::class;
+    protected static ?string $model = Lesson::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-collection';
 
     protected static ?string $navigationGroup = MenuTitles::CATEGORY_APP;
-    protected static ?string $pluralModelLabel = 'Анонсы';
-    protected static ?string $modelLabel = 'Анонс';
+
+    protected static ?string $pluralModelLabel = 'Уроки';
+
+    protected static ?string $modelLabel = 'Урок';
+
 
     public static function form(Form $form): Form
     {
@@ -46,8 +49,11 @@ class AnnounceResource extends Resource
                     ->multiple()
                     ->relationship('tags', 'name')
                     ->searchable(),
-                DateTimePicker::make('release_at')->minDate(now()),
-                Checkbox::make('main')
+                TextInput::make('rating')
+                    ->integer()
+                    ->minValue(1)
+                    ->maxValue(5)
+                    ->maxLength(255),
             ]);
     }
 
@@ -56,22 +62,13 @@ class AnnounceResource extends Resource
         return $table
             ->columns([
                 TextColumn::make('title'),
-                TextColumn::make('description'),
+                // TextColumn::make('description'),
                 TextColumn::make('video_path')
                     ->tooltip(fn($record) => $record->video_path)
                     ->limit(15),
                 ImageColumn::make('preview_path'),
-                // TextColumn::make('tags'),
-                IconColumn::make('main')
-                    ->boolean()
-                    ->trueIcon('heroicon-o-check-circle')
-                    ->trueColor('success')
-                    ->falseIcon('heroicon-o-ban')
-                    ->falseColor('danger')
-                    ->alignCenter(),
-                TextColumn::make('release_at')
-                    ->dateTime()
-                    ->sortable(),
+                TextColumn::make('tags')->avg('tags', 'name'),
+                TextColumn::make('rating'),
                 TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
@@ -86,27 +83,25 @@ class AnnounceResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\ViewAction::make(),
-                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
-
+    
     public static function getRelations(): array
     {
         return [
             //
         ];
     }
-
+    
     public static function getPages(): array
     {
         return [
-            'index'  => Pages\ListAnnounces::route('/'),
-            'create' => Pages\CreateAnnounce::route('/create'),
-            'edit'   => Pages\EditAnnounce::route('/{record}/edit'),
+            'index' => Pages\ListLessons::route('/'),
+            'create' => Pages\CreateLesson::route('/create'),
+            'edit' => Pages\EditLesson::route('/{record}/edit'),
         ];
-    }
+    }    
 }
