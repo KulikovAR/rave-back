f31a6fa7e663b55a9e41d1d3e5c08d84ae8ab41e
Any Task <30393521+cartoonsub@users.noreply.github.com>
2023-09-07 22:03:52 +0300

    #SCHOOL-193.BE Админ. Добавление и редактирование баннера на главной

--- tests/Feature/BannerTest.php
+++ tests/Feature/BannerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Models\Banner;
 
 class BannerTest extends TestCase
 {
@@ -21,9 +22,11 @@ class BannerTest extends TestCase
 
     public function test_get_img_by_action_url(): void
     {
+        $banner = Banner::factory()->create();
+
         $response = $this->json(
             'get',
-            route('banner.index', ['action_url' => 'officia-iure-reiciendis-expedita-illum']),
+            route('banner.index', ['action_url' => $banner->action_url]),
             headers: $this->getHeadersForUser()
         );
 
