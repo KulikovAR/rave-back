e1dbe0f4bd414deb3ce8dcb6fd02f47c94444c8a
Boris Wild <boriswild@gmail.com>
2023-09-13 18:39:02 +0300

    add order by

--- app/Filament/Resources/ProposalResource.php
+++ app/Filament/Resources/ProposalResource.php
@@ -21,17 +21,17 @@ class ProposalResource extends Resource
 {
     protected static ?string $model = Proposal::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
-    protected static ?string $navigationGroup = MenuTitles::CATEGORY_APP;
+    protected static ?string $navigationIcon   = 'heroicon-o-collection';
+    protected static ?string $navigationGroup  = MenuTitles::CATEGORY_APP;
     protected static ?string $pluralModelLabel = 'Предложения';
-    protected static ?string $modelLabel = 'Предложения';
+    protected static ?string $modelLabel       = 'Предложения';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
-            ]);
+                         //
+                     ]);
     }
 
     public static function table(Table $table): Table
@@ -39,43 +39,44 @@ class ProposalResource extends Resource
         return $table
             ->columns([
 
-                TextColumn::make('body')
-                    ->tooltip(fn($record) => $record->body)
-                    ->limit(15),
-                ImageColumn::make('file'),
-                IconColumn::make('unread')
-                    ->boolean()
-                    ->trueIcon('heroicon-o-ban')
-                    ->trueColor('danger')
-                    ->falseIcon('heroicon-o-check-circle')
-                    ->falseColor('success')
-                    ->alignCenter(),
-                TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-            ])
+                          TextColumn::make('body')
+                                    ->tooltip(fn($record) => $record->body)
+                                    ->limit(15),
+                          ImageColumn::make('file'),
+                          IconColumn::make('unread')
+                                    ->boolean()
+                                    ->trueIcon('heroicon-o-ban')
+                                    ->trueColor('danger')
+                                    ->falseIcon('heroicon-o-check-circle')
+                                    ->falseColor('success')
+                                    ->alignCenter(),
+                          TextColumn::make('created_at')
+                                    ->dateTime()
+                                    ->sortable()
+                                    ->toggleable(isToggledHiddenByDefault: true),
+                          TextColumn::make('updated_at')
+                                    ->dateTime()
+                                    ->sortable()
+                                    ->toggleable(isToggledHiddenByDefault: true),
+                      ])
+            ->defaultSort('updated_at', 'desc')
             ->filters([
-                //
-            ])
+                          //
+                      ])
             ->actions([
-                Tables\Actions\Action::make('read')
-                ->action(fn (Proposal $record) => $record->update(['unread' => false])),
-                Tables\Actions\Action::make('user')->url(
-                    fn(Proposal $record): string => UserResource::getUrl('edit', ['record' => $record->user])
-                )
-                    ->openUrlInNewTab(),
-                Tables\Actions\ViewAction::make(),
-                Tables\Actions\DeleteAction::make(),
-                
-            ])
+                          Tables\Actions\Action::make('read')
+                                               ->action(fn(Proposal $record) => $record->update(['unread' => false])),
+                          Tables\Actions\Action::make('user')->url(
+                              fn(Proposal $record): string => UserResource::getUrl('edit', ['record' => $record->user])
+                          )
+                                               ->openUrlInNewTab(),
+                          Tables\Actions\ViewAction::make(),
+                          Tables\Actions\DeleteAction::make(),
+
+                      ])
             ->bulkActions([
-                Tables\Actions\DeleteBulkAction::make(),
-            ]);
+                              Tables\Actions\DeleteBulkAction::make(),
+                          ]);
     }
 
     public static function getRelations(): array
