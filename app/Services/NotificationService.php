b92a9211355ad4bc3a4b5131193b525bef30265d
Boris Wild <boriswild@gmail.com>
2023-09-14 16:08:53 +0300

    add notifications

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Models\Role;
+use App\Models\User;
 use App\Notifications\AdminNotification;
 use Illuminate\Support\Facades\Notification;
 
@@ -12,5 +14,8 @@ class NotificationService
         Notification::route('mail', [
             config('site-values.email_support.email_support') => 'Admin',
         ])->notify(new AdminNotification($message));
+
+        $admin = User::role(Role::ROLE_ADMIN)->get()->first();
+        $admin->notify(new AdminNotification($message));
     }
 }
