705227f72fb202c2b758dd412bb26bc47fde2157
Boris Wild <boriswild@gmail.com>
2023-09-24 15:53:56 +0300

    upd comments admin section, add payment mails.

--- app/Filament/Resources/CommentResource/RelationManagers/CommentsRelationManager.php
+++ app/Filament/Resources/CommentResource/RelationManagers/CommentsRelationManager.php
@@ -3,15 +3,10 @@
 namespace App\Filament\Resources\CommentResource\RelationManagers;
 
 use App\Filament\Resources\CommentResource;
-use App\Models\Comment;
-use Filament\Forms;
 use Filament\Resources\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Resources\Table;
-use Filament\Tables;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Illuminate\Support\Facades\Auth;
+use Filament\Tables\Actions\CreateAction;
 
 class CommentsRelationManager extends RelationManager
 {
@@ -21,44 +16,12 @@ class CommentsRelationManager extends RelationManager
 
     public static function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                Forms\Components\Select::make('user_id')
-                    ->relationship('user', 'email')
-                    ->searchable(['id', 'email'])
-                    ->default(Auth::user()->id)
-                    ->required(),
-                Forms\Components\TextInput::make('body')
-                    ->required()
-                    ->maxLength(255),
-            ]);
+        return CommentResource::form($form);
     }
 
     public static function table(Table $table): Table
     {
-        return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('user.id'),
-                Tables\Columns\TextColumn::make('user.userProfile.firstname'),
-                Tables\Columns\TextColumn::make('user.userProfile.lastname'),
-                Tables\Columns\TextColumn::make('nesting_comments_count')->counts('nesting_comments'),
-                Tables\Columns\TextColumn::make('body'),
-            ])
-            ->filters([
-                //
-            ])
-            ->headerActions([
-                Tables\Actions\CreateAction::make(),
-            ])
-            ->actions([
-                Tables\Actions\Action::make('edit')->url(
-                    fn(Comment $record): string => CommentResource::getUrl('edit', $record)
-                )
-                    ->openUrlInNewTab(),
-                Tables\Actions\DeleteAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\DeleteBulkAction::make(),
-            ]);
+        return CommentResource::table($table)
+            ->appendHeaderActions([CreateAction::make()]);
     }
 }
