5dcabb4e24fbfd83c4ac0d1c7c935bbd0b345623
SashkaBackEnd <106818844+SashkaBackEnd@users.noreply.github.com>
2023-09-29 15:21:40 +0300

    Поправил уведомления

--- app/Http/Resources/Notification/NotificationResource.php
+++ app/Http/Resources/Notification/NotificationResource.php
@@ -19,7 +19,7 @@ class NotificationResource extends JsonResource
         return [
             'id'         => $this->id,
             'data'       => $this->data,
-            'read_at'    => $this->readed_at,
+            'read_at'    => $this->read_at,
             'created_at' => $this->formatDateTimeForOutput($this->created_at)
         ];
     }
