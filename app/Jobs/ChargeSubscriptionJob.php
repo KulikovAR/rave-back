f17699a5659e935bf48caf8198299d2de27c52c6
Boris Wild <boriswild@gmail.com>
2024-01-17 18:53:44 +0300

    add cancel payment

--- app/Jobs/ChargeSubscriptionJob.php
+++ app/Jobs/ChargeSubscriptionJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Enums\EnvironmentTypeEnum;
 use App\Http\Controllers\PaymentController;
 use App\Models\Order;
 use App\Models\User;
@@ -12,6 +13,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 
 class ChargeSubscriptionJob implements ShouldQueue
@@ -31,7 +33,6 @@ class ChargeSubscriptionJob implements ShouldQueue
      */
     public function handle(): void
     {
-        Log::info('Subscription charging job...');
 
         $users = User::inRandomOrder()
                      ->where('subscription_expires_at', '<=', Carbon::now())
@@ -39,7 +40,10 @@ class ChargeSubscriptionJob implements ShouldQueue
                      ->limit(50)
                      ->get();
 
-        Log::info(print_r($users, true));
+        if($users &&  App::environment(EnvironmentTypeEnum::notProductEnv())){
+            Log::info('Subscription charging job...');
+            Log::info(print_r($users, true));
+        }
 
         foreach ($users as $user) {
             $order = $user->orders()->where(['order_status' => Order::PAYED])->first();
