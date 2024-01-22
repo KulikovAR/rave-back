c564f03219f9bb77b6ee2351167b32d8f1b1c126
Boris Wild <boriswild@gmail.com>
2024-01-22 18:27:26 +0300

    add shorts id to response

--- app/Http/Resources/Shorts/ShortResource.php
+++ app/Http/Resources/Shorts/ShortResource.php
@@ -17,6 +17,7 @@ class ShortResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id'          => $this->id,
             'title'       => $this->title,
             'slide_count' => $this->slides()->count(),
             'view_count'  => $this->view_count,
