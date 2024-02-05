38266db8e389234b3ee6fccb0179ff96115ebdf5
SashkaBackEnd <106818844+SashkaBackEnd@users.noreply.github.com>
2024-02-05 21:55:03 +0300

    SCHOOL-250 правка запроса posqresql

--- app/Filament/Widgets/UserDeviceOverview.php
+++ app/Filament/Widgets/UserDeviceOverview.php
@@ -25,14 +25,15 @@ class UserDeviceOverview extends Widget
 
     private function getUserList(): Collection
     {
-        return User::whereHas('roles', function ($role) {
-            $role->where('name', Role::ROLE_USER);
-        })
-            ->with('userProfile')
-            ->withCount('userDevices')
-            ->having('user_devices_count','>', self::USER_DEVICE_COUNT_LIMIT)
-            ->orderBy('user_devices_count')
-            ->limit(10)
+        return User::select('users.*')
+            ->whereHas('roles', function ($role) {
+                $role->where('name', Role::ROLE_USER);
+            })
+            ->selectRaw('COUNT(user_devices.id) as user_devices_count')
+            ->leftJoin('user_devices', 'users.id', '=', 'user_devices.user_id')
+            ->groupBy('users.id')
+            ->havingRaw('COUNT(user_devices.id) > 5')
+            ->orderBy('user_devices_count', 'desc')
             ->get();
     }
 }
